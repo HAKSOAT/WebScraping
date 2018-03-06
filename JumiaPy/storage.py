0553c4f0c1befefe67220683d0d8222187cbdad8
RealHaks <shopejuh@gmail.com>
2018-03-06 22:39:48 +0100

    Storage.py file created, with __init__ method written

---
+++ JumiaPy/storage.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Excel(object):
+    """
+    
+    
+    
+    """
+    def __init__(self, names, links, prices, ratings, rated_sales, sellers):
+        
+        self.names = []
+        self.links = []
+        self.prices = []
+        self.ratings = []
+        self.rated_sales = []
+        self.sellers = []
+        
