217364e35d3fd488cab56d65e6c727fc7159e164
RealHaks <shopejuh@gmail.com>
2018-03-07 06:49:56 +0100

    output and make_tables method added to excel class

--- JumiaPy/storage.py
+++ JumiaPy/storage.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 class Excel(object):
@@ -8,10 +9,21 @@ class Excel(object):
     """
     def __init__(self, names, links, prices, ratings, rated_sales, sellers):
         
-        self.names = []
-        self.links = []
-        self.prices = []
-        self.ratings = []
-        self.rated_sales = []
-        self.sellers = []
+        self.names = names
+        self.links = links
+        self.prices = prices
+        self.ratings = ratings
+        self.rated_sales = rated_sales
+        self.sellers = sellers
+        self.pd_dataframe = []
+        
+    def make_tables(self):
+        
+        pd_dataframe = pd.DataFrame({"Names": self.names, "Links": self.links, "Prices": self.prices,
+                                    "Ratings": self.ratings, "Rated Sales": self.rated_sales,
+                                    "Sellers": self.sellers})
+    def output(self):
+        writer = pd.ExcelWriter("output.xlsx")
+        pd_dataframe.to_excel(writer, "Sheet1", index = False)
+        writer.save()
         
