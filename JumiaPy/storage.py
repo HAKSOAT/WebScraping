41be1bb292ec1e2c671b263e60fc6b0fb5c08661
RealHaks <shopejuh@gmail.com>
2018-03-07 10:01:42 +0100

    insert_row method written

--- JumiaPy/storage.py
+++ JumiaPy/storage.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import sqlite3
 
@@ -39,18 +38,44 @@ class Database(object):
         self.ratings = ratings
         self.rated_sales = rated_sales
         self.sellers = sellers
-        self.connect_status = None 
+        self.connect_status = None
+        
+        #Database table and column names
+        self.table_name = "JUMIA PRODUCTS"
+        self.index_column = "ID"
+        self.column_1 = "NAMES"
+        self.column_2 = "LINKS"
+        self.column_3 = "PRICES"
+        self.column_4 = "RATINGS"
+        self.column_5 = "RATED SALES"
+        self.column_6 = "SELLERS"
 
     def create(self):
+        
         new_database = sqlite3.connect("output.sqlite3")
         new_database.close()
         
     def connect(self):
+        
         self.connect_status = sqlite3.connect("output.sqlite3")
         return self.connect_status.cursor()
     
     def make_table(self, access):
         
         access.execute("CREATE TABLE {} ({} INTEGER AUTOINCREMENT PRIMARY KEY, {} TEXT, {} TEXT, {} REAL,\
-                        {} REAL,{} INTEGER, {} TEXT);".format(self.table_name, self.column_1, self.column_2,
-                                                              self.column_3, self.column_4, self.column_5, self.column_6))
+                        {} REAL,{} INTEGER, {} TEXT);".format(self.table_name, self.index_column,
+                                                              self.column_1, self.column_2, 
+                                                              self.column_3, self.column_4, 
+                                                              self.column_5, self.column_6))
+        
+    def insert_row(self, access):
+        
+        for name, link, price, rating, rated_sale, seller in zip(self.names,self.links ,self.prices,
+                                                                 self.ratings, self.rated_sales, self.sellers):
+            
+            access.execute("INSERT INTO {} ({} , {} , {} , {}, {}, {}, {})\
+                            VALUES ({} , {} , {} , {}, {}, {}, {});".format(self.table_name, self.column_1, 
+                                                                            self.column_2, self.column_3, 
+                                                                            self.column_4, self.column_5, 
+                                                                            self.column_6, name, link, price, 
+                                                                            rating, rated_sale, seller))
